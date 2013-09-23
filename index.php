37ba73257390ac42061fa2f471e4a8906d861ddc
Yvan Lopez <yvanchus@gmail.com>
2013-09-23 18:51:13 -0500

    Demos de JTable

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <ol>
+            <li>
+                <a href="Codes/jTableSimple.php">JTable Simple</a>
+            </li>
+            <li>
+                <a href="Codes/jTableSimplePagedSorted.php">JTable Paged Sorted</a>
+            </li>
+        </ol>        
+    </body>
+</html>
